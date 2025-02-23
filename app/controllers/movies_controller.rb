8661903ba19e78296552772761445499f1d06d79
SGuarnizo <sguarniz@chicagobooth.edu>
2025-02-23 01:19:12 +0000

    fin

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,4 +14,41 @@ class MoviesController < ApplicationController
 
     render({ :template => "movie_templates/show" })
   end
+
+  def insert
+    @m = Movie.new
+    @m.title = params.fetch("movie_title_param")
+    @m.year = params.fetch("movie_year_param")
+    @m.duration = params.fetch("movie_duration_param")
+    @m.description = params.fetch("movie_description_param")
+    @m.image = params.fetch("movie_image_param")
+    @m.director_id = params.fetch("movie_director_param")
+    @m.save
+
+    redirect_to("/movies")
+  end
+
+  def update
+    the_id = params.fetch("movie_id")
+    matching_movie = Movie.where({ :id => the_id })
+    @m = matching_movie.at(0)
+    @m.title = params.fetch("mupdate_title_param")
+    @m.year = params.fetch("mupdate_year_param")
+    @m.duration = params.fetch("mupdate_duration_param")
+    @m.description = params.fetch("mupdate_description_param")
+    @m.image = params.fetch("mupdate_image_param")
+    @m.director_id = params.fetch("mupdate_director_param")
+    @m.save
+
+    redirect_to("/movies/#{@m.id}")
+  end
+
+  def delete
+    the_id = params.fetch("mov_id")
+    matching_movies = Movie.where({ :id => the_id })
+    @m= matching_movies.at(0)
+    @m.destroy
+
+    redirect_to("/actors")
+  end
 end
